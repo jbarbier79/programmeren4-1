1d670fca26a7edc2062597fc6eecce7db22ae3c0
wimrigo <wim.rigo@gmail.com>
2013-10-14 21:07:28 +0200

    test prog4

--- oef2/src/test/test.java
+++ oef2/src/test/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Oefening GitHub2");
+		System.out.println("Oefening GitHub3");
 
 	}
 
